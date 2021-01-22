61a6ad71a2c22ce5e9f145fdc96850bbd3be7ac3
GeonNamGung <rndrjs123@naver.com>
2021-01-22 19:11:52 +0900

    네비게이션 스타일 변경

--- components/MenuList/MenuList.js
+++ components/MenuList/MenuList.js
@@ -1,4 +1,4 @@
-import { AppBar, Menu, MenuItem, Toolbar, makeStyles, ListItem, ListItemIcon, ListItemAvatar, Avatar, ListItemText, List } from "@material-ui/core";
+import { AppBar, Menu, MenuItem, Toolbar, makeStyles, ListItem, ListItemIcon, ListItemAvatar, Avatar, ListItemText, List, Divider } from "@material-ui/core";
 import FolderIcon from "@material-ui/icons/Folder";
 import { toJS } from "mobx";
 import { observer } from "mobx-react-lite";
@@ -31,34 +31,35 @@ const MenuList = ({ className, style }) => {
       <List>
         <Link href="/">
           <ListItem button>
-            <ListItemIcon>
-              <FolderIcon />
-            </ListItemIcon>
             <ListItemText primary={"홈"} />
           </ListItem>
         </Link>
+        <Divider />
         <Link href="/theme">
           <ListItem button>
-            <ListItemIcon>
-              <FolderIcon />
-            </ListItemIcon>
             <ListItemText primary={"테마 목록"} />
           </ListItem>
         </Link>
         <Link href="/theme/event">
           <ListItem button>
-            <ListItemIcon>
-              <FolderIcon />
-            </ListItemIcon>
             <ListItemText primary={"테마 일정"} />
           </ListItem>
         </Link>
+        <Divider />
+        <Link href="/stocks">
+          <ListItem button>
+            <ListItemText primary={"종목 정리"} />
+          </ListItem>
+        </Link>
+        <Link href="/stocks/event">
+          <ListItem button>
+            <ListItemText primary={"종목 일정"} />
+          </ListItem>
+        </Link>
+        <Divider />
         {auth.isLogined && (
           <Link href="/sign/out">
             <ListItem button>
-              <ListItemIcon>
-                <FolderIcon />
-              </ListItemIcon>
               <ListItemText primary={"로그아웃"} />
             </ListItem>
           </Link>
---
+++ components/MultiLines/MultiLines.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MultiLines = ({ lines, style, className }) => {
+  return (
+    <div style={style} className={className}>
+      {lines
+        .split("\n")
+        .filter((line) => line)
+        .map((line) => {
+          return (
+            <p className="explain" key={line}>
+              {line}
+            </p>
+          );
+        })}
+    </div>
+  );
+};
+
+export default MultiLines;
